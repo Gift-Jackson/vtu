6074c80ca78af42e94963f649c7f26061b1fe57d
Gift Jacksun <giftjacksun@gmail.com>
2024-05-11 05:48:28 +0100

    Updated UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,49 @@
-import styles from '../styles/home.module.css'
+import { Link, NavLink } from "react-router-dom";
+import styles from "../styles/header.module.css";
 const Header = () => {
   return (
-      <>
+    <>
       <header className={styles.header}>
         <a href="/">
           <h1 className={styles.brand}>QuestPay</h1>
         </a>
 
-        <a href="https://github.com/Gift-Jackson/vtu" target='_blank'>
-          <i className="fa-brands fa-github"></i> Github
-        </a>
-      </header>
-      </>
-  )
-}
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <NavLink className="nav-link" to="/">
+                Welcome
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/offers">
+                Offers
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/contacts">
+                Reach Us
+              </NavLink>
+            </li>
+          </ul>
 
-export default Header
+          <div className={styles.btns}>
+            <a href="https://github.com/Gift-Jackson/vtu" target="_blank">
+              <i className="fa-brands fa-github"></i> <span>Github</span>
+            </a>
+            <Link to="/connect">
+              <button className={styles.btn}>
+                Let me in... <i className="fa-solid fa-arrow-right"></i>
+              </button>
+            </Link>
+            <button className="menu-btn">
+              <span className="material-symbols-outlined">menu</span>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/HeroWrapper.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import heroSvg from "../assets/hero.png";
+import { ReactTyped } from "react-typed";
+import style from '../styles/hero.module.css'
+
+const HeroWrapper = () => {
+  return (
+    <>
+      <main>
+        <div className={style.container}>
+          <div className={style.left}>
+            <div className={style.content}>
+              <p>Bill payment got easier</p>
+              <h1>
+                We <span className={style.mark}>offer</span>{" "} <br />
+                <ReactTyped
+                  strings={[
+                    "cheap data.",
+                    "cheap airtime.",
+                    "and more...",
+                  ]}
+                  typeSpeed={100}
+                  backSpeed={50}
+                  loop
+                />
+              </h1>
+              <p>
+                              Join <span className="mark">QuestPay</span> Today... <br />
+                             Purchase data, airtime and Utility bills, all on super cheap rates.
+              </p>
+              <Link to="/connect">
+                <button className={style.btn}>
+                  Get Started <i className="fa-solid fa-arrow-right"></i>
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className={style.right}>
+                      <div className={style.img}>
+                      <img src={heroSvg} alt="Hero illustration" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default HeroWrapper;
